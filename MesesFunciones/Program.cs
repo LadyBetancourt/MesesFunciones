e5279f83993b7d5a60afea3e9964af98c1d19691
LadyBetancourt <lady_betancourt24@unikino.edu.mx>
2024-11-11 07:59:09 -0700

    Agregar archivos de proyecto.

---
+++ MesesFunciones/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace MesesFunciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MostrarMensaje("Hello world");
+            MostrarMensaje("Introduce un número");
+            string mes = ObtenerMes(3);
+            MostrarMensaje(mes);
+        }
+
+
+        static void MostrarMensaje(string mensaje)
+
+        static string ObtenerMes(int numeroMes)
+        {
+
+            switch (numeroMes)
+            {
+                case 1: return "Enero";
+                case 2: return "Febrero";
+                case 3: return "Marzo";
+                case 4: return "Abril";
+                case 5: return "Mayo";
+                case 6: return "Junio";
+                case 7: return "Julio";
+                case 8: return "Agosto";
+                case 9: return "Septiembre";
+                case 10: return "Octubre";
+                case 11: return "Noviembre";
+                case 12: return "Diciembre";
+                    
+                default return "Número incorrecto";
+
+            }
+
+        }
+    }
+}
